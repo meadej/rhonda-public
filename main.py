e0e549abb3d699daa722f5b42559ba978e081bd4
Jon Meade <meade.jonathan@gmail.com>
2017-12-27 20:13:28 -0700

    Added adverbs and hashtags

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import random
 import twitter
+from datetime import datetime
 
 def tweet(tweet_string):
 	api = twitter.Api(consumer_key='',
@@ -15,7 +16,7 @@ def tweet(tweet_string):
 	return 0	
 
 def main():
-	number_items_to_return = 3
+	number_items_to_return = 4
 	try:
 		request_response = requests.get("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=" + str(number_items_to_return) + "&format=json")
 		response_json = request_response.json()
@@ -27,13 +28,19 @@ def main():
 	for response_item in response_json['query']['random']:
 		titles.append(response_item['title'])
 	
-	join_words = []
+	verbs = []
+	adverbs = []
 
 	fhandle = open("verbs.txt", 'r')	
 	for line in fhandle.readlines():
-		join_words.append(line)
-	
-	output = titles[0] + " and " + titles[1] + " " + join_words[random.randint(0, len(join_words) - 1)].strip() + " " + titles[2]
+		verbs.append(line)
+
+	fhandle.close()
+	fhandle = open("adverbs.txt", 'r')
+	for line in fhandle.readlines():
+		adverbs.append(line)
+
+	output = "On this day in " + str(random.randint(1800, int(datetime.now().year))) + ": " + titles[1] + " " + adverbs[random.randint(0, len(adverbs) - 1)].strip() + " " + verbs[random.randint(0, len(verbs) - 1)].strip() + " " + titles[2] + " #" + str(titles[3]).replace(" ", "").replace(",","").replace("(","").replace(")","").replace("-","").replace("'", "").replace('"','')
 	tweet(output)
 	return 0
 
